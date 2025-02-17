3d5178b97f2916e385d1cd442972f342c6c30734
Richardluiz <richardluizborba@hotmail.com>
2025-02-17 12:58:00 -0300

    update itemList

--- front-end/src/components/Main.jsx
+++ front-end/src/components/Main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ItemList from "/src/components/ItemList.jsx";
+import ItemList from "./ItemList";
 import { artistArray } from "../Assets/database/artists";
 import { songsArray } from "../Assets/database/songs";
 
