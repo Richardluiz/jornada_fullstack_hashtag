72eb2fd5e2ab9bd25c08adc54d2ddcbbe9629073
Richardluiz <richardluizborba@hotmail.com>
2025-02-17 12:18:43 -0300

    Atualizando o deploy

--- front-end/src/pages/Artist.jsx
+++ front-end/src/pages/Artist.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 import { Link, useParams } from "react-router-dom";
 import SongList from "../components/SongList";
-import { artistArray } from "../assets/database/artists";
+import { artistArray } from "../Assets/database/artists";
 import { songsArray } from "../assets/database/songs";
 
 const Artist = () => {
